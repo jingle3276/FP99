73e8c293a5db9cac796ed4a920978ed644c3f34b
Ying Zhao <ying.zhao@yzhao-m.jet.local>
2017-06-07 10:07:57 -0400

    add flattern list

--- Functional99.fsx
+++ Functional99.fsx
@@ -35,3 +35,52 @@ let rec reverse' acc aList =
     | [] -> acc
     | head :: tail -> reverse' (head::acc) tail 
 
+
+// Find the number of elements of a list. (easy)
+// Bonus for a tail recursive solution.
+let length aList = 
+    let rec aux acc aList = 
+        match aList with 
+        | [] -> acc
+        | _::tail -> aux (acc + 1 ) tail
+    aux 0 aList
+
+
+
+// Flatten a nested list structure. (medium)
+// (* There is no nested list type in OCaml(FSharp), so we need to define one
+//      first. A node of a nested list is either an element, or a list of
+//      nodes. *)
+//   type 'a node =
+//     | One of 'a 
+//     | Many of 'a node list;;
+// type 'a node = One of 'a | Many of 'a node list
+type Node<'a> = 
+    | One of 'a                // Flatterned case
+    | Many of Node<'a> list    // Nested case
+
+let l1 = One 3  //simple list:  One 3
+let l2 = Many [l1; One 5]  // one level nested:  Many [One 3; One 5]
+let l3 = Many [l2]  // two levels nested:  Many [Many [One 3; One 5]]
+
+// # flattern [ One "a" ; Many [ One "b" ; Many [ One "c" ; One "d" ] ; One "e" ] ];;
+// - : string list = ["a"; "b"; "c"; "d"; "e"]
+let flattern (data: Node<'a> list): 'a list =
+    let rec aux arr (data: Node<'a> list): 'a list = 
+        match data with
+        | [] -> arr
+        | head::tail ->
+            match head with 
+            | One v -> aux (v::arr) tail
+            | Many l -> aux (aux arr l) tail
+    aux [] data |> List.rev
+
+let flattern2 (data: Node<'a> list): 'a list =
+    let rec aux arr (data: Node<'a> list): 'a list =
+        match data with 
+        | [] -> arr
+        | One v::tail -> aux (v::arr) tail
+        | Many l::tail -> aux (aux arr l) tail
+    aux [] data |> List.rev
+
+
